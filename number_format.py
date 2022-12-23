9835f80cb53c6e2918caa3937d2beb54e16aa297
ianroddrs <mateusroddi@gmail.com>
2022-12-23 19:57:54 -0300

    retorno

--- number_format.py
+++ number_format.py
@@ -1,15 +1,20 @@
 numer = f"R$ {2.4:07.2f}"
-print(numer)
+print(numer) # R$ 0002.40
 
 numer = f"R$ {221.3214:7.2f}"
-print(numer)
+print(numer) # R$  221.32
 
 numer = f"R$ {3214:7d}"
-print(numer)
+print(numer) # R$    3214
 
 numer = f"R$ {3214:07d}"
-print(numer)
+print(numer) # R$ 0003214
 
 data = f"Data {9:02d}/{4:02d}"
 
-print(data)
+print(data) # Data 09/04
+
+
+
+
+
