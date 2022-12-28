53ae9d2bd996d7f903c0b2324eac1a7ac3b38d32
ianroddrs <mateusroddi@gmail.com>
2022-12-28 03:44:43 -0300

    Duck typing

--- object_orientation_2/modelo.py
+++ object_orientation_2/modelo.py
@@ -44,20 +44,48 @@ class Serie(Programa):
     def __str__(self):
         return f"{self.nome} - {self.ano} - {self.temporadas} temp : {self.likes} Likes"
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.__programas = programas
+
+    def __getitem__(self, item):
+        return self.__programas[item]
+
+    def __len__(self):
+        return len(self.__programas)
+
+    @property
+    def listagem(self):
+        return self.__programas
+
+    
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 
 vingadores.dar_like()
-
 atlanta.dar_like()
 atlanta.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+tmep.dar_like()
 
 
 # print(f"{vingadores.nome} - {vingadores.ano} - {vingadores.duracao} : {vingadores.likes}")
 # print(f"{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas} : {atlanta.likes}")
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f"Tamanho da Playlist: {len(playlist_fim_de_semana)}")
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f"Está na playlist? {demolidor in playlist_fim_de_semana}")
+
+print(playlist_fim_de_semana[2])
